9bed2d8e17b460686830683eb449306cff819d64
csettles <caitsettles@gmail.com>
2020-03-14 12:03:09 -0700

    Add fr4

--- InnReservations.java
+++ InnReservations.java
@@ -35,6 +35,46 @@ public class InnReservations {
    }
 
    /*
+    * Prompts the user for a reservation code and deletes the row.
+    */
+   private void cancelReservation() throws SQLException {
+      try (Connection conn = DriverManager.getConnection(System.getenv("HP_JDBC_URL"),
+                                                                 System.getenv("HP_JDBC_USER"),
+                                                                 System.getenv("HP_JDBC_PW"))) {
+         String deleteSql = "DELETE FROM " + RESERVATIONS_TABLE + "WHERE Code = ?";
+         String confirmSql = "SELECT FROM " + RESERVATIONS_TABLE + "WHERE Code = ?";
+
+         Scanner scanner = new Scanner(System.in);
+         System.out.println("Enter the reservation code you would like to cancel: ");
+         String code = scanner.nextLine();
+
+         try (Statement stmt = conn.createStatement();
+               ResultSet rs = stmt.executeQuery(confirmSql);) {
+            while (rs.next()) {
+               System.out.format("\tDeleting reservation for %s %s...\n", rs.getString("Firstname"), rs.getString("Lastname"));
+            }
+         }
+
+         System.out.print("Are you sure? (y/n)");
+         String confirmation = scanner.nextLine().toLower();
+         if (confirmation != "y") {
+            System.out.prinln("Goodbye.");
+            return;
+         }
+
+         conn.setAutoCommit(false);
+         try (PreparedStatement stmt = conn.prepareStatement(deleteSql)) {
+            stmt.setString(1, code);
+            int rowCount = stmt.executeUpdate();
+            System.out.format("Updated %d records for reservation %s\n", rowCount, code);
+            conn.commit();
+         } catch (SQLException e) {
+            conn.rollback();
+         }
+      }
+   }
+
+   /*
     * Prints all rows/columns from the specified table.
     */
    private static void example(String table) throws SQLException {
