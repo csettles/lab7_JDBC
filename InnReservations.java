4a121b77715436302d385903e6f129bb8e92e5a6
Steven Gandham <ssgandha@calpoly.edu>
2020-03-17 12:54:12 -0700

    Working FR6 :)

--- InnReservations.java
+++ InnReservations.java
@@ -12,13 +12,14 @@ import java.time.LocalDate;
 import java.util.List;
 import java.util.ArrayList;
 
-/*
--- Shell init:
+
+/*-- Shell init:
 export CLASSPATH=$CLASSPATH:mysql-connector-java-8.0.16.jar:.
-export HP_JDBC_URL=jdbc:mysql://db.labthreesixfive.com/your_username_here?autoReconnect=true\&useSSL=false
-export HP_JDBC_USER=
-export HP_JDBC_PW=
- */
+export HP_JDBC_URL=jdbc:mysql://db.labthreesixfive.com/ssgandha?autoReconnect=true\&useSSL=false
+export HP_JDBC_USER=ssgandha
+export HP_JDBC_PW="WinterTwenty20_365_012926780"
+*/
+ 
 public class InnReservations {
    private static void setup() {
       try {
@@ -34,41 +35,169 @@ public class InnReservations {
       try (Connection conn = DriverManager.getConnection(System.getenv("HP_JDBC_URL"),
                                                                  System.getenv("HP_JDBC_USER"),
                                                                  System.getenv("HP_JDBC_PW"))) {
-         Scanner scanner = new Scanner(System.in);
-         System.out.print("Find pastries with price <=: ");
-         Double price = Double.valueOf(scanner.nextLine());
-         System.out.print("Filter by flavor (or 'Any'): ");
-         String flavor = scanner.nextLine();
+         StringBuilder sb = new StringBuilder();
 
-         List<Object> params = new ArrayList<Object>();
-         params.add(price);
-         StringBuilder sb = new StringBuilder("SELECT * FROM hp_goods WHERE price <= ?");
-         if (!"any".equalsIgnoreCase(flavor)) {
-             sb.append(" AND Flavor = ?");
-             params.add(flavor);
-         }
+         /* build R6 query */
          
-         try (PreparedStatement pstmt = conn.prepareStatement(sb.toString())) {
-             int i = 1;
-             for (Object p : params) {
-                 pstmt.setObject(i++, p);
-             }
+         sb.append("WITH NotSameMonth AS (");
+         sb.append("                        SELECT Code, Room, CheckIn, CheckOut, ");
+         sb.append("                        MONTHNAME(CheckIn) AS Month1,");
+         sb.append("                        MONTHNAME(CheckOut) AS Month2,");
+         sb.append("                        DATEDIFF(LAST_DAY(CheckIn), CheckIn)+1 AS Month1Total,");
+         sb.append("                        DATEDIFF(CheckOut, DATE_ADD(LAST_DAY(CheckIn), INTERVAL 1 DAY)) AS Month2Total, ");
+         sb.append("                        Rate");
+         sb.append("                        FROM shbae.lab7_reservations R");
+         sb.append("                        WHERE MONTHNAME(CheckIn) <> MONTHNAME(CheckOut)");
+         sb.append("                        ),");
+         sb.append("SameMonth AS (");
+         sb.append("                    SELECT Code, Room, CheckIn, CheckOut,");
+         sb.append("                    MONTHNAME(CheckIn) AS Month1, ");
+         sb.append("                    DATEDIFF(CheckOut, CheckIn) AS Month1Total, ");
+         sb.append("                    Rate");
+         sb.append("                    FROM shbae.lab7_reservations R");
+         sb.append("                    WHERE MONTHNAME(CheckIn) = MONTHNAME(CheckOut)");
+         sb.append("                    ),");
+         sb.append("AllRoomsAndMonths AS (");
+         sb.append("                    SELECT Room, MonthName, ROUND(SUM(MonthTotal), 0) AS MonthTotal");
+         sb.append("                    FROM (SELECT Room, Month1 AS MonthName, SUM(Rate*Month1Total) AS MonthTotal");
+         sb.append("                            FROM NotSameMonth");
+         sb.append("                            GROUP BY Room, Month1");
+         sb.append("                            ");
+         sb.append("                            UNION ALL");
+         sb.append("                ");
+         sb.append("                            (SELECT Room, Month2 AS MonthName, SUM(Rate*Month2Total) AS MonthTotal");
+         sb.append("                            FROM NotSameMonth");
+         sb.append("                            GROUP BY Room, Month2) ");
+         sb.append("                            ");
+         sb.append("                            UNION ALL");
+         sb.append("                            ");
+         sb.append("                            (SELECT Room, Month1 AS MonthName, SUM(Rate*Month1Total) AS MonthTotal");
+         sb.append("                            FROM SameMonth");
+         sb.append("                            GROUP BY Room, Month1) ");
+         sb.append("                            ) AS ALLMONTHS");
+         sb.append("                    GROUP BY Room, MonthName");
+         sb.append("                    )");
+         sb.append("SELECT Room, ");
+         sb.append("SUM(CASE ");
+         sb.append("    WHEN MonthName = 'January' THEN MonthTotal ELSE 0 ");
+         sb.append("END) AS January,");
+         sb.append("SUM(CASE ");
+         sb.append("    WHEN MonthName = 'February' THEN MonthTotal ELSE 0 ");
+         sb.append("END) AS February,");
+         sb.append("SUM(CASE ");
+         sb.append("    WHEN MonthName = 'March' THEN MonthTotal ELSE 0 ");
+         sb.append("END) AS March,");
+         sb.append("SUM(CASE ");
+         sb.append("    WHEN MonthName = 'April' THEN MonthTotal ELSE 0 ");
+         sb.append("END) AS April,");
+         sb.append("SUM(CASE ");
+         sb.append("    WHEN MonthName = 'May' THEN MonthTotal ELSE 0 ");
+         sb.append("END) AS May,");
+         sb.append("SUM(CASE ");
+         sb.append("    WHEN MonthName = 'June' THEN MonthTotal ELSE 0 ");
+         sb.append("END) AS June,");
+         sb.append("SUM(CASE ");
+         sb.append("    WHEN MonthName = 'July' THEN MonthTotal ELSE 0 ");
+         sb.append("END) AS July,");
+         sb.append("SUM(CASE ");
+         sb.append("    WHEN MonthName = 'August' THEN MonthTotal ELSE 0 ");
+         sb.append ("END) AS August,");
+         sb.append("SUM(CASE ");
+         sb.append("    WHEN MonthName = 'September' THEN MonthTotal ELSE 0 ");
+         sb.append("END) AS September,");
+         sb.append("SUM(CASE ");
+         sb.append("    WHEN MonthName = 'October' THEN MonthTotal ELSE 0 ");
+         sb.append("END) AS October,");
+         sb.append("SUM(CASE ");
+         sb.append("    WHEN MonthName = 'November' THEN MonthTotal ELSE 0 ");
+         sb.append("END) AS November,");
+         sb.append("SUM(CASE ");
+         sb.append("    WHEN MonthName = 'December' THEN MonthTotal ELSE 0 ");
+         sb.append("END) AS December,");
+         sb.append("SUM(MonthTotal) AS YearlyTotal ");
+         sb.append("FROM AllRoomsAndMonths ");
+         sb.append("GROUP BY Room ");
+         sb.append("UNION ALL ");
+         sb.append("SELECT 'Totals' AS Totals, ");
+         sb.append("SUM(CASE ");
+         sb.append("    WHEN MonthName = 'January' THEN MonthTotal ELSE 0 ");
+         sb.append("END) AS January,");
+         sb.append("SUM(CASE ");
+         sb.append("    WHEN MonthName = 'February' THEN MonthTotal ELSE 0 ");
+         sb.append("END) AS February,");
+         sb.append("SUM(CASE ");
+         sb.append("    WHEN MonthName = 'March' THEN MonthTotal ELSE 0 ");
+         sb.append("END) AS March,");
+         sb.append("SUM(CASE ");
+         sb.append("    WHEN MonthName = 'April' THEN MonthTotal ELSE 0 ");
+         sb.append("END) AS April,");
+         sb.append("SUM(CASE ");
+         sb.append("    WHEN MonthName = 'May' THEN MonthTotal ELSE 0 ");
+         sb.append("END) AS May,");
+         sb.append("SUM(CASE ");
+         sb.append("    WHEN MonthName = 'June' THEN MonthTotal ELSE 0 ");
+         sb.append("END) AS June,");
+         sb.append("SUM(CASE ");
+         sb.append("    WHEN MonthName = 'July' THEN MonthTotal ELSE 0 ");
+         sb.append("END) AS July,");
+         sb.append("SUM(CASE ");
+         sb.append("    WHEN MonthName = 'August' THEN MonthTotal ELSE 0 ");
+         sb.append("END) AS August,");
+         sb.append("SUM(CASE ");
+         sb.append("    WHEN MonthName = 'September' THEN MonthTotal ELSE 0 ");
+         sb.append("END) AS September,");
+         sb.append("SUM(CASE ");
+         sb.append("    WHEN MonthName = 'October' THEN MonthTotal ELSE 0 ");
+         sb.append("END) AS October,");
+         sb.append("SUM(CASE ");
+         sb.append("    WHEN MonthName = 'November' THEN MonthTotal ELSE 0 ");
+         sb.append("END) AS November,");
+         sb.append("SUM(CASE ");
+         sb.append("    WHEN MonthName = 'December' THEN MonthTotal ELSE 0 ");
+         sb.append("END) AS December,");
+         sb.append("SUM(MonthTotal) AS YearlyTotal ");
+         sb.append("FROM AllRoomsAndMonths ");
+         sb.append("ORDER BY Room;");
 
-             try (ResultSet rs = pstmt.executeQuery()) {
-                 System.out.println("Matching Pastries:");
+            try (Statement stmt = conn.createStatement()){
+
+              try (ResultSet rs = stmt.executeQuery(sb.toString())) {
                  int matchCount = 0;
-                 while (rs.next()) {
-                     System.out.format("%s %s ($%.2f) %n", rs.getString("Flavor"), rs.getString("Food"), rs.getDouble("price"));
-                     matchCount++;
+                 System.out.format("| %10s | %10s | %10s | %10s | %10s | %10s | %10s | %10s | %10s | %10s | %10s | %10s | %10s | %10s |\n", 
+                 "Room", "January", "February", "March", "April", "May", "June", "July", "August",
+                 "September", "October", "November", "December", "YearTotal");
+                 int i = 0;
+                 for (i = 0; i < 180; i++){
+                    System.out.print("-");
                  }
-                 System.out.format("----------------------%nFound %d match%s %n", matchCount, matchCount == 1 ? "" : "es");
-             }
-         }
+                 System.out.print("\n");
+                 while (rs.next()) {
+                    System.out.format("| %10s | %10s | %10s | %10s | %10s | %10s | %10s | %10s | %10s | %10s | %10s | %10s | %10s | %10s |\n", 
+                    rs.getString("Room"), rs.getInt("January"), rs.getInt("February"), 
+                    rs.getInt("March"), rs.getInt("April"), rs.getInt("May"), rs.getInt("June"), 
+                    rs.getInt("July"), rs.getInt("August"), rs.getInt("September"), 
+                    rs.getInt("October"), rs.getInt("November"), rs.getInt("December"), 
+                    rs.getInt("YearlyTotal"));
+                    matchCount++;
+                 }
+                 for (i = 0; i < 14; i++){
+                    System.out.print("+------------");
+                 }
+                 System.out.println("+\n");
+              }
+           }
       }
    }
 
    public static void main(String[] arg) {
       System.out.println("Hello, World!");
       setup();
+
+      try {
+         InnReservations IR = new InnReservations();
+         IR.example();
+      } catch (SQLException e) {
+          System.err.println("SQLException: " + e.getMessage());
+      }
    }
 }
